a692f3befc1d79a598e4a03fd81c805bc8c46e1d
Axxtur3l <agrenardo@brooklynsteamcenter.org>
2022-12-15 10:51:57 -0500

    value counter

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 file = open('NASA_access_log_Jul95')
 
 ip_address = []
+files = []
+time = []
+
 
 try:
     for line in file:
         ip_address.append(line.split(" ")[0])
+        files.append(line.split(" ")[6])
+        time.append(line.split(" ")[3].split(":")[0])
+
 except:
         print("uh-oh")
 
-print(ip_address)
+from collections import Counter
+
+a = Counter(ip_address).most_common(1)
+b = Counter(files).most_common(1)
+c = Counter(time).most_common(1)
+print(a)
+print(b)
+print(c)
+
